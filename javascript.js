8f9a7d61448c9e4cfa0027a4c249f802af2ed337
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-04-29 11:27:57 -0300

    Added a grid toggle but slider doesn't work

--- javascript.js
+++ javascript.js
@@ -1,31 +1,48 @@
-const GRIDSIDE = 600;
-let squaresPerSide = 16;
+const gridWidth = getComputedStyle(document.body).getPropertyValue("--grid-width");
+const inactiveColor = getComputedStyle(document.body).getPropertyValue("--inactive-color");
+const accentColor = getComputedStyle(document.body).getPropertyValue("--accent-color");
 
+
+const container = document.querySelector("#container");
 const sketchArea = document.querySelector("#sketch-area");
-const sliderContainer = document.querySelector("#slider-container");
 const slider = document.querySelector("#slider");
 const sliderValue = document.querySelector("#slider-value");
 
-sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`
-sketchArea.style.width = sketchArea.style.height = `${GRIDSIDE}px`;
+const gridToggle = document.querySelector("#grid-toggle");
+
+let squaresPerSide = 16;
+let gridVisible = false;
+
+function toggleGrid() {
+    gridVisible = gridVisible ? false : true;
+    gridToggle.style.color = gridVisible ? accentColor : inactiveColor;
+
+    removeGridCells();
+    createGridCells();
+}
 
 function setBackgroundColor () {
     this.style.backgroundColor = "black";
 }
-function createGridCells(squaresPerSide) {
-    const numOfSquares = (squaresPerSide * squaresPerSide)
-    const widthOrHeight = `${(GRIDSIDE / squaresPerSide) - 2}px`
+function createGridCells() {
+    const numOfSquares = (squaresPerSide * squaresPerSide);
 
     for (let i = 0; i < numOfSquares; i++) {
         const gridCell = document.createElement("div");
 
-        gridCell.style.width = gridCell.style.height = widthOrHeight;
-        gridCell.classList.add("cell");
+        if (gridVisible) {
+            widthOrHeight = `${(parseInt(gridWidth) / squaresPerSide) - 2}px`;
+            gridCell.style.border = "1px solid whitesmoke";
+        } else if (!gridVisible) {
+            widthOrHeight = `${(parseInt(gridWidth) / squaresPerSide)}px`;
+            gridCell.style.border = "none";
+        }
 
-        sketchArea.appendChild(gridCell);
+        gridCell.style.width = gridCell.style.height = widthOrHeight;
 
         gridCell.addEventListener("mouseover", setBackgroundColor);
 
+        sketchArea.appendChild(gridCell);
     }
 } 
 
@@ -42,4 +59,6 @@ slider.oninput = function () {
     createGridCells(this.value);
 }
 
-createGridCells(16);
+gridToggle.addEventListener("click", toggleGrid);
+
+createGridCells();
